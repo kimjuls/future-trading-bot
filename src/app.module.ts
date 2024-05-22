5deb5a082671c6383d11a042163000c1dade49db
ilovenewts <iloveamalamute@gmail.com>
2024-05-23 08:50:01 +0900

    changed order-manager module name

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,18 @@ import { AppService } from './app.service';
 import { CandleCollectorModule } from './candle-collector/candle-collector.module';
 import { StrategyModule } from './strategy/strategy.module';
 import { CandleAnalyzerModule } from './candle-analyzer/candle-analyzer.module';
-import { OrderExecutorModule } from './order-executor/order-executor.module';
 import { MyLoggerModule } from './my-logger/my-logger.module';
 import { RiskManagerModule } from './risk-manager/risk-manager.module';
+import { OrderManagerModule } from './order-manager/order-manager.module';
 
 @Module({
   imports: [
     CandleCollectorModule,
     StrategyModule,
     CandleAnalyzerModule,
-    OrderExecutorModule,
     MyLoggerModule,
     RiskManagerModule,
+    OrderManagerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
