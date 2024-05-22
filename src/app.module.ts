bc3d1c2499dbacc67169c9cd510dff5b3bdcddcd
ilovenewts <iloveamalamute@gmail.com>
2024-05-22 12:55:02 +0900

    set initial structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CandleCollectorModule } from './candle-collector/candle-collector.module';
+import { StrategyModule } from './strategy/strategy.module';
+import { CandleAnalyzerModule } from './candle-analyzer/candle-analyzer.module';
+import { OrderExecutorModule } from './order-executor/order-executor.module';
+import { MyLoggerModule } from './my-logger/my-logger.module';
+import { RiskManagerModule } from './risk-manager/risk-manager.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CandleCollectorModule,
+    StrategyModule,
+    CandleAnalyzerModule,
+    OrderExecutorModule,
+    MyLoggerModule,
+    RiskManagerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
