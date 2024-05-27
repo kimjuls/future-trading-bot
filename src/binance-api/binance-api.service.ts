ff700fcc4ff77ec716df0a038dfaa530044c6f87
ilovenewts <iloveamalamute@gmail.com>
2024-05-27 14:50:17 +0900

    implement a signing function

--- src/binance-api/binance-api.service.ts
+++ src/binance-api/binance-api.service.ts
@@ -2,10 +2,11 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ApiKey } from './classes/api-key';
 import { ConfigService } from '@nestjs/config';
+import { createHmac } from 'crypto';
 
 @Injectable()
 export class BinanceApiService {
-  private readonly apiKey: ApiKey;
+  private readonly apiKey: ApiKey = null;
 
   constructor(
     private readonly httpService: HttpService,
@@ -18,5 +19,15 @@ export class BinanceApiService {
       apiKey.access = this.configService.getOrThrow<string>('BINANCE_ACCESS');
       apiKey.secret = this.configService.getOrThrow<string>('BINANCE_SECRET');
     }
+  }
+
+  private sign(params?: any) {
+    this.configure();
+    const queryString = new URLSearchParams(params).toString();
+    const signature = createHmac('sha256', this.apiKey.secret)
+                 .update(queryString)
+                 .digest('hex');
+    params.signature = signature;
+    return params;
   }
 }
