a7abde88061f02af212a9663b6478cbebfd05a03
ilovenewts <iloveamalamute@gmail.com>
2024-06-17 13:35:54 +0900

    added an API endpoint fetchCandles

--- src/binance-api/binance-api.service.spec.ts
+++ src/binance-api/binance-api.service.spec.ts
@@ -1,11 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BinanceApiService } from './binance-api.service';
+import { FetchCandlesDto } from './dto/fetch-candles.dto';
+import { Interval } from './enums/interval.enum';
+import { lastValueFrom, map } from 'rxjs';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 describe('BinanceApiService', () => {
   let service: BinanceApiService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), HttpModule],
       providers: [BinanceApiService],
     }).compile();
 
@@ -14,5 +20,13 @@ describe('BinanceApiService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('', async () => {
+    const dto = new FetchCandlesDto();
+    dto.symbol = 'BTCUSDT';
+    dto.interval = Interval.OneHour;
+    const res = await service.fetchCandles(dto);
+    expect(res.status).toEqual(200);
   });
 });
